3a13282c7b65495073a4db9811858514175b074a
sqoshi <!piter68>
2021-07-28 18:43:56 +0200

    added download test + tox/ pytest config

---
+++ tests/test_predictor_download.py
@@ -0,0 +1,22 @@
+import os
+from unittest import TestCase
+from unittest.mock import patch
+
+from mask_imposer.detector.download import download_predictor
+
+from logging import getLogger
+
+
+def get_directory_files(path):
+    return [f for f in os.listdir(path) if os.path.isfile(f)]
+
+
+class PredictorDownloadTestCase(TestCase):
+
+    # noinspection PyTypeChecker
+    @patch("mask_imposer.detector.download.input", lambda _: "y")
+    def test_should_download_predictor(self):
+        download_predictor(getLogger("test"), "SPZ.bz2")
+        self.assertTrue("SPZ.dat" in get_directory_files("."))
+        os.remove("SPZ.dat")
+        os.remove("SPZ.bz2")
