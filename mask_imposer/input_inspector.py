64f536055b19dfd1ee4eb0b427b252601f32f28c
sqoshi <!piter68>
2021-07-29 23:44:58 +0200

    landmark detection finished

--- mask_imposer/input_inspector.py
+++ mask_imposer/input_inspector.py
@@ -16,19 +16,19 @@ class Inspector:
     """
 
     def __init__(self, logger: ColoredLogger):
-        self.logger = logger
-        self.images: List[str] = []
+        self._logger = logger
+        self._images: List[str] = []
 
     def _find_images(self, path: str) -> List[str]:
         """Find images in given path recognized by their extensions [filename.extension]."""
         all_files = listdir(path)
         if not all_files:
-            self.logger.critical(f"{path} is empty.")
+            self._logger.critical(f"{path} is empty.")
             sys.exit()
-        return [f for f in listdir(path) if ImageFormat.is_image(join(path, f))]
+        return [join(path, f) for f in listdir(path) if ImageFormat.is_image(join(path, f))]
 
     def get_images(self) -> List[str]:
-        return self.images
+        return self._images
 
     def inspect(self, directory_path: str) -> None:
         """Inspect directory in path.
@@ -39,13 +39,15 @@ class Inspector:
         """
 
         if not isdir(directory_path):
-            self.logger.error(f"{directory_path} is not a directory.")
+            self._logger.error(f"{directory_path} is not a directory.")
             sys.exit()
 
-        self.images = self._find_images(directory_path)
+        self._images = self._find_images(directory_path)
 
-        if not self.images:
-            self.logger.critical(f"Images not found in {directory_path}.")
+        if not self._images:
+            self._logger.critical(f"Images not found in {directory_path}.")
             sys.exit()
         else:
-            self.logger.info(f"Found {len(self.images)} in {directory_path}.")
+            self._logger.info(f"Found {len(self._images)} images in {directory_path}.")
+
+        self._logger.info("Inspection finished.")
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, Namespace
 
 from mask_imposer.colored_logger import get_configured_logger
 from mask_imposer.definitions import ImageFormat
+from mask_imposer.detector.landmark_detector import Detector
 from mask_imposer.input_inspector import Inspector
 
 
@@ -11,8 +12,12 @@ def _parse_args() -> Namespace:
     parser.add_argument("input_dir", type=str, help="Input directory.")
     # parser.add_argument("--input-dir", type=str, default=None, help="Input directory.")
     parser.add_argument("--output-dir", type=str, default="results", help="Output directory.")
-    parser.add_argument("--target-ext", choices=list(ImageFormat), type=ImageFormat, default="png",
+    parser.add_argument("--output-format", choices=list(ImageFormat), type=ImageFormat, default="png",
                         help="Output images format.")
+    parser.add_argument("--shape-predictor", type=str, default=None, help="Path to shape predictor.")
+    parser.add_argument("--show-samples", type=bool, default=False, help="Show sample after detection.")
+    parser.add_argument("--detect-face-boxes", type=bool, default=False,
+                        help="Before landmark prediction detect face box.")
     return parser.parse_args()
 
 
@@ -22,4 +27,10 @@ def main():
     # logger.propagate = False
     # logger.disabled = True
     args = _parse_args()
-    Inspector(logger).inspect(args.input_dir)
+
+    inspector = Inspector(logger)
+    inspector.inspect(args.input_dir)
+
+    detector = Detector(inspector.get_images(), args.shape_predictor, args.detect_face_boxes, args.show_samples, logger)
+    detector.detect()
+    # detector.save(args.output_dir, args.output_format)
