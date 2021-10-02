78bd0409d28c21f43059f4f05f288cdcce0eb2a3
sqoshi <!piter68>
2021-10-02 13:29:53 +0200

    fixed some pylint errors

--- mask_imposer/collector/coords_collector.py
+++ mask_imposer/collector/coords_collector.py
@@ -52,7 +52,7 @@ class CoordinatesCollector:
         cv2.destroyAllWindows()
         if not confirmed:
             return self.reset()
-        self._logger.info(f"Coordinates accepted by user.")
+        self._logger.info("Coordinates accepted by user.")
         return self.mask_coords
 
     def _assign_mask_coords(self) -> None:
--- tests/test_mask_image_read.py
+++ tests/test_mask_image_read.py
@@ -1,13 +1,13 @@
-from logging import getLogger
-from unittest import TestCase
-
-from mask_imposer.imposer.mask_imposer import Imposer
-
-
-class MaskImageReadTestCase(TestCase):
-    @classmethod
-    def setUp(cls) -> None:
-        cls.imposer = Imposer({}, getLogger("test"))
-
-    # def test_should_read_mask_image(self):
-    #     self.imposer._read_mask_image()
+# from logging import getLogger
+# from unittest import TestCase
+#
+# from mask_imposer.imposer.mask_imposer import Imposer
+#
+#
+# class MaskImageReadTestCase(TestCase):
+#     @classmethod
+#     def setUp(cls) -> None:
+#         cls.imposer = Imposer({}, getLogger("test"))
+#
+#     # def test_should_read_mask_image(self):
+#     #     self.imposer._read_mask_image()
