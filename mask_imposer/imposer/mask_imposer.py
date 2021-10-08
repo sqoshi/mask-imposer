f4fbcac053583735868f2d15cf24e234c1f387ed
sqoshi <!piter68>
2021-10-08 21:34:26 +0200

    fixed lint errors ++

--- mask_imposer/imposer/mask_imposer.py
+++ mask_imposer/imposer/mask_imposer.py
@@ -103,9 +103,7 @@ class Imposer:
                         * target_image.img[left_top_point.y:, left_top_point.x:, c]
                 )
             elif surplus.h and not surplus.w:
-                target_image.img[
-                left_top_point.y: -surplus.h, left_top_point.x:, c
-                ] = (
+                target_image.img[left_top_point.y: -surplus.h, left_top_point.x:, c] = (
                         only_mask
                         + alpha_l
                         * target_image.img[
@@ -113,9 +111,7 @@ class Imposer:
                           ]
                 )
             elif not surplus.h and surplus.w:
-                target_image.img[
-                left_top_point.y:, left_top_point.x: -surplus.w, c
-                ] = (
+                target_image.img[left_top_point.y:, left_top_point.x: -surplus.w, c] = (
                         only_mask
                         + alpha_l
                         * target_image.img[
@@ -123,9 +119,7 @@ class Imposer:
                           ]
                 )
             elif surplus.h and surplus.w:
-                target_image.img[
-                left_top_point.y: -surplus.h, left_top_point.x: -surplus.w, c
-                ] = (
+                target_image.img[left_top_point.y: -surplus.h, left_top_point.x: -surplus.w, c] = (
                         only_mask
                         + alpha_l
                         * target_image.img[
