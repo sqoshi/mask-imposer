4332c83fad65b33917b2d72e2eafce396ef07b35
sqoshi <!piter68>
2021-10-12 20:03:45 +0200

    requirements.txt update for new package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Mask Imposer",
-    version="1.2.1",
+    version="2.0.0",
     description="Tool to overlay fake face masks.",
     url="https://github.com/sqoshi/mask-imposer",
     author="Piotr Popis",
@@ -18,10 +18,10 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        "wheel==0.36.2",
-        "termcolor==1.1.0",
-        "dlib==19.22.0",
-        "progressbar==2.5",
+        "wheel>=0.36.2",
+        "termcolor>=1.1.0",
+        "dlib>=19.22.0",
+        "progressbar>=2.5",
         "opencv-python>=4.5.3.5"
     ],
 )
